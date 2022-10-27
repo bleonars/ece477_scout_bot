b2f56afb4f0c1f697ba8f016fb0544afc459f2dd
bleonars <bleonars@purdue.edu>
2022-10-26 20:56:50 -0400

    hardware timer pwm

---
+++ scout_bot_receiver/src/bdc_motor.cpp
@@ -0,0 +1,56 @@
+#include "bdc_motor.h"
+
+void bdc_motor_setup(bdc_motor_config_t *bdc_config) {
+    // https://docs.espressif.com/projects/esp-idf/en/v4.4.2/esp32/api-reference/peripherals/mcpwm.html#configure
+    mcpwm_config_t     pwm_config;
+    mcpwm_pin_config_t pin_config;
+    
+    if (bdc_config->m_mcpwm_unit == MCPWM_UNIT_0) {
+        pin_config.mcpwm0a_out_num = bdc_config->m_mcpwm_out_gpios[0];
+        pin_config.mcpwm0b_out_num = bdc_config->m_mcpwm_out_gpios[1];
+    }
+    else if (bdc_config->m_mcpwm_unit == MCPWM_UNIT_1) {
+        pin_config.mcpwm1a_out_num = bdc_config->m_mcpwm_out_gpios[0];
+        pin_config.mcpwm1b_out_num = bdc_config->m_mcpwm_out_gpios[1];
+    }
+    
+    mcpwm_set_pin(bdc_config->m_mcpwm_unit, &pin_config);
+    
+    pwm_config.frequency    = VEX393_PWM_BASE_FREQ;
+    pwm_config.cmpr_a       = 0;
+    pwm_config.cmpr_b       = 0;
+    pwm_config.duty_mode    = MCPWM_DUTY_MODE_0;
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    
+    mcpwm_init(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, &pwm_config);
+}
+
+void bdc_motor_drive(bdc_motor_config_t *bdc_config) {
+    mcpwm_start(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer);
+}
+
+void bdc_motor_stop(bdc_motor_config_t *bdc_config) {
+    // https://docs.espressif.com/projects/esp-idf/en/v4.4.2/esp32/api-reference/peripherals/mcpwm.html#operate
+    mcpwm_stop(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer);
+}
+
+void bdc_motor_set_speed(bdc_motor_config_t *bdc_config, float duty_cycle_percentage) {
+    /*mcpwm_set_signal_low(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_A);*/
+    /*mcpwm_set_signal_low(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_B);*/
+
+    /* motor moves forward, with duty cycle = duty % */
+    if (duty_cycle_percentage > 0) {
+        mcpwm_set_duty(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_A, duty_cycle_percentage);
+        mcpwm_set_duty(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_B, duty_cycle_percentage);
+        mcpwm_set_duty_type(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_A, MCPWM_DUTY_MODE_1);
+        mcpwm_set_duty_type(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_B, MCPWM_DUTY_MODE_0);
+    }
+
+     /* motor moves backward, with duty cycle = -duty % */
+    else {
+        mcpwm_set_duty(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_A, -1 * duty_cycle_percentage);
+        mcpwm_set_duty(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_B, -1 * duty_cycle_percentage);
+        mcpwm_set_duty_type(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_A, MCPWM_DUTY_MODE_0);
+        mcpwm_set_duty_type(bdc_config->m_mcpwm_unit, bdc_config->m_mcpwm_timer, MCPWM_GEN_B, MCPWM_DUTY_MODE_1);
+    }
+}
--- scout_bot_receiver/src/main.cpp
+++ scout_bot_receiver/src/main.cpp
@@ -1,22 +1,25 @@
 #include "rfmgr_srv.h"
 #include "payload.h"
 
+#include "bdc_motor.h"
+
 Utils::LogManager g_log_mgr;
 ScoutBot_Server::RFManager_Server g_rfmgr_srv;
+bdc_motor_config_t motor_a_cfg;
 
 void setup() {
     g_log_mgr.allocate(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, 1024, 0, 115200);
     g_log_mgr.println("scout bot receiver v1.0");
+
+    motor_a_cfg.m_mcpwm_out_gpios[0] = 2; 
+    motor_a_cfg.m_mcpwm_out_gpios[1] = 4; 
+    motor_a_cfg.m_mcpwm_unit         = MCPWM_UNIT_0;
+    motor_a_cfg.m_mcpwm_timer        = MCPWM_TIMER_0;
     
-    pinMode(2, OUTPUT);
-    pinMode(15, OUTPUT);
+    bdc_motor_setup(&motor_a_cfg);
+    bdc_motor_drive(&motor_a_cfg);
+    bdc_motor_set_speed(&motor_a_cfg, 20);
 }
 
 void loop() {
-    digitalWrite(2, HIGH);
-    digitalWrite(15, LOW);
-    delay(1000);
-    digitalWrite(2, LOW);
-    digitalWrite(15, HIGH);
-    delay(1000);
 }
