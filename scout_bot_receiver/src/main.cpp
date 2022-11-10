fb39ee393d6f83032a10459b037873aa7c6c2ef5
bleonars <bleonars@purdue.edu>
2022-11-10 15:59:09 -0500

    integrated controls code, cleaned up main, added some notes on how to implement drive code

--- scout_bot_receiver/src/main.cpp
+++ scout_bot_receiver/src/main.cpp
@@ -3,75 +3,88 @@
 #include "main.h"
 #include "bdc_motor.h"
 
-Utils::LogManager g_log_mgr;
-ScoutBot_Server::RFManager_Server g_rfmgr_srv;
-bdc_motor_config_t motor_a_cfg;
+Utils::LogManager                         g_log_mgr;
+ScoutBot_Server::RFManager_Server         g_rfmgr_srv;
 
-ChassisControl chassis; 
+static std::array<bdc_motor_config_t, 4>  motor_cfg;
+static ChassisControl                     chassis_ctrl; 
+
+void setup_characteristics(ScoutBot_Server::RFManager_Service *receiver_service) {
+    static float    motor_zero_duty   = 0.f; 
+    static uint32_t range_enable_off  = 0;
+    static uint32_t drive_mode_arcade = 0;
+
+    auto left_joy_y_char = receiver_service->add_characteristic(BLEUUID(LEFT_JS_Y_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    left_joy_y_char->setValue(motor_zero_duty);
+
+    auto right_joy_y_char = receiver_service->add_characteristic(BLEUUID(RIGHT_JS_Y_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    right_joy_y_char->setValue(motor_zero_duty);
+
+    auto right_joy_x_char = receiver_service->add_characteristic(BLEUUID(RIGHT_JS_X_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    right_joy_x_char->setValue(motor_zero_duty);
+
+    auto range_en_char = receiver_service->add_characteristic(BLEUUID(RANGE_ENABLE_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    range_en_char->setValue(range_enable_off);
+
+    auto drive_mode_char = receiver_service->add_characteristic(BLEUUID(DRIVE_MODE_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+    drive_mode_char->setValue(drive_mode_arcade);
+}
+
+void setup_motors() {
+    motor_cfg[0].m_mcpwm_out_gpios[0] = 5; 
+    motor_cfg[0].m_mcpwm_out_gpios[1] = 19; 
+    motor_cfg[0].m_mcpwm_unit         = MCPWM_UNIT_0;
+    motor_cfg[0].m_mcpwm_timer        = MCPWM_TIMER_0;
+    bdc_motor_setup(&motor_cfg[0]);
+
+    motor_cfg[1].m_mcpwm_out_gpios[0] = 19; 
+    motor_cfg[1].m_mcpwm_out_gpios[1] = 21; 
+    motor_cfg[1].m_mcpwm_unit         = MCPWM_UNIT_0;
+    motor_cfg[1].m_mcpwm_timer        = MCPWM_TIMER_0;
+    bdc_motor_setup(&motor_cfg[1]);
+   
+    motor_cfg[2].m_mcpwm_out_gpios[0] = 22; 
+    motor_cfg[2].m_mcpwm_out_gpios[1] = 23; 
+    motor_cfg[2].m_mcpwm_unit         = MCPWM_UNIT_1;
+    motor_cfg[2].m_mcpwm_timer        = MCPWM_TIMER_1;
+    bdc_motor_setup(&motor_cfg[2]);
+    
+    motor_cfg[3].m_mcpwm_out_gpios[0] = 26; 
+    motor_cfg[3].m_mcpwm_out_gpios[1] = 27; 
+    motor_cfg[3].m_mcpwm_unit         = MCPWM_UNIT_1;
+    motor_cfg[3].m_mcpwm_timer        = MCPWM_TIMER_1;
+    bdc_motor_setup(&motor_cfg[3]);
+}
 
 void setup() {
     g_log_mgr.allocate(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, 1024, 0, 115200);
     g_log_mgr.println("scout bot receiver v1.0");
 
     g_rfmgr_srv.setup("scout bot receiver v1.0");
-
-    static uint32_t val = 0;
-    static uint32_t check = 123;
-    auto service = g_rfmgr_srv.add_service(BLEUUID(SERVICE_UUID));
-    auto characteristic = service->add_characteristic(BLEUUID(CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(check); // sanity check to make sure this is working
-    characteristic = service->add_characteristic(BLEUUID(LEFT_JS_Y_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(val);
-    characteristic = service->add_characteristic(BLEUUID(RIGHT_JS_Y_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(val);
-    characteristic = service->add_characteristic(BLEUUID(RIGHT_JS_X_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(val);
-    characteristic = service->add_characteristic(BLEUUID(RANGE_ENABLE_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(val);
-    characteristic = service->add_characteristic(BLEUUID(DRIVE_MODE_CHARACTERISTIC_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    characteristic->setValue(val);
-
-    g_rfmgr_srv.adv_start();
-
-    delay(100);
-    auto motor_1_duty_cycle_char = service->add_characteristic(BLEUUID(MOTOR_1_DUTY_UUID), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    static float motor_1_duty_cycle = 0.f;
-    motor_1_duty_cycle_char->setValue(motor_1_duty_cycle);
+    
+    auto receiver_service = g_rfmgr_srv.add_service(BLEUUID(SERVICE_UUID));
+    setup_characteristics(receiver_service);
 
     g_rfmgr_srv.adv_start();
     
-    motor_a_cfg.m_mcpwm_out_gpios[0] = 19; 
-    motor_a_cfg.m_mcpwm_out_gpios[1] = 21; 
-    motor_a_cfg.m_mcpwm_unit         = MCPWM_UNIT_0;
-    motor_a_cfg.m_mcpwm_timer        = MCPWM_TIMER_0;
-    bdc_motor_setup(&motor_a_cfg);
+    setup_motors();
 }
 
 void loop() {
-    auto service = g_rfmgr_srv.get_service(BLEUUID(SERVICE_UUID));
+    auto receiver_service = g_rfmgr_srv.get_service(BLEUUID(SERVICE_UUID));
     
-    // TODO: retrieve data from characteristic table
-    auto characteristic = service->get_characteristic(BLEUUID(CHARACTERISTIC_UUID));
-    uint32_t leftY = Utils::payload_to_u32(service->get_characteristic(BLEUUID(LEFT_JS_Y_CHARACTERISTIC_UUID))->getData());
-    uint32_t rightY = Utils::payload_to_u32(service->get_characteristic(BLEUUID(RIGHT_JS_Y_CHARACTERISTIC_UUID))->getData());
-    uint32_t rightX = Utils::payload_to_u32(service->get_characteristic(BLEUUID(RIGHT_JS_X_CHARACTERISTIC_UUID))->getData());
+    float left_joy_y  = Utils::payload_to_f32(receiver_service->get_characteristic(BLEUUID(LEFT_JS_Y_CHARACTERISTIC_UUID))->getData());
+    float right_joy_y = Utils::payload_to_f32(receiver_service->get_characteristic(BLEUUID(RIGHT_JS_Y_CHARACTERISTIC_UUID))->getData());
+    float right_joy_x = Utils::payload_to_f32(receiver_service->get_characteristic(BLEUUID(RIGHT_JS_X_CHARACTERISTIC_UUID))->getData());
     
-    bool rangeEnabled = Utils::payload_to_u32(service->get_characteristic(BLEUUID(RANGE_ENABLE_CHARACTERISTIC_UUID))->getData());
-    bool driveMode = Utils::payload_to_u32(service->get_characteristic(BLEUUID(DRIVE_MODE_CHARACTERISTIC_UUID))->getData());
+    uint32_t range_enbled = Utils::payload_to_u32(receiver_service->get_characteristic(BLEUUID(RANGE_ENABLE_CHARACTERISTIC_UUID))->getData());
+    uint32_t drive_mode   = Utils::payload_to_u32(receiver_service->get_characteristic(BLEUUID(DRIVE_MODE_CHARACTERISTIC_UUID))->getData());
 
-    uint32_t range = chassis.get_range();
+    uint32_t range = chassis_ctrl.get_range();
 
-    // TODO: Potentially also add redundant protection for motors if BLE is not connected.
-    
-
-    // g_log_mgr.println("dbg: characteristic value: " + std::to_string(Utils::payload_to_u32(characteristic->getData())));
-    //
-    auto motor_1_duty_cycle_char = service->get_characteristic(BLEUUID(MOTOR_1_DUTY_UUID));
-    float joystick_data = Utils::payload_to_f32(motor_1_duty_cycle_char->getData());
-    g_log_mgr.println(std::to_string(joystick_data));
-
-    bdc_motor_drive(&motor_a_cfg);
-    bdc_motor_set_speed(&motor_a_cfg, joystick_data);
-
-    delay(10);
+    /* based on range detection mode, active drive mode, and current joystick readings perform drive code actions        */
+    /* notes; each joystick is read with [-100, 100] range. 0 reading indicates joystick is in the middle (no action)    */
+    /*      ; write this value to motor_set_speed. remember, we have to arrange the motor direction correctly, so that   */
+    /*      ; correct motors move backwards or forwards based on the direction we want to go... might have to negate the */
+    /*      ; value read from joysticks accordingly                                                                      */
 }
